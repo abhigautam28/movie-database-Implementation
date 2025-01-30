0607a6c75a5d8e269bc6a78af49ad26e1634825b
Abhishek Gautam <abhigautam172000@gmail.com>
2025-01-31 00:10:05 +0530

    added admin module src file

---
+++ admin/src/main/java/com/movie/database/AdminApplication.java
@@ -0,0 +1,14 @@
+package com.movie.database;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApplication implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
